09f57d35d65b6a7df49d69d8e1910306332a3a63
Justin Li <justinli4002@gmail.com>
2023-07-20 12:28:55 -0500

    logistic regression successful! though poor performance

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 merger_path = 'src\\data\\mergers.xlsx'
 
@@ -40,4 +42,13 @@ y = data['Label']
 # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None)
 
-print(X_train)
+
+# https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
+model = LogisticRegression()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
