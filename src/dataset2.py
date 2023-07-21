afd3f076b1c82c2412cc385f8c213b142a0449f6
Justin Li <justinli4002@gmail.com>
2023-07-20 23:38:41 -0500

    figuring out why only predicting nonmergers

--- src/dataset2.py
+++ src/dataset2.py
@@ -12,6 +12,8 @@ def preprocess(data):
     data['is_closed'] = data['is_closed'].astype(int)
     # data = data.drop(columns=['is_closed'])
     
+    data['is_acquired'] = data['is_acquired'].astype(int)
+    
     return data
 
 if __name__ == '__main__':
@@ -19,17 +21,26 @@ if __name__ == '__main__':
     data = pd.read_csv(data_path)
     
     data = data.drop(columns=[
-        'company_id'
+        'products_number',
+        'company_id',
+        'acquired_companies',
+        'mba_degree',
+        'phd_degree',
+        'ms_degree',
+        'other_degree',
     ])
     
     data = preprocess(data)
+    
     na_count_per_row = data.isna().sum(axis=1)
-
     # Total number of rows with NaN values
     total_rows_with_na = len(na_count_per_row[na_count_per_row > 0])
     print("Total rows with NaN values:", total_rows_with_na)
     print(len(data))
     data = data.dropna()
+    print(len(data))
+    df2 = len(data[data["is_acquired"]==True])
+    print('Num Acquired: ' + str(df2))
     
     predictors = data.columns.tolist()
     predictors.remove('is_acquired')
@@ -38,10 +49,9 @@ if __name__ == '__main__':
     y = data['is_acquired']
     
     # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=None)
-
-
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None)
+    
     # https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
-    model = LogisticRegression(max_iter=500)
+    model = RandomForestClassifier()
 
     run_model(model, 'Logistic', X_train, X_test, y_train, y_test, predictors)
