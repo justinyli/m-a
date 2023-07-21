889147e7f4ec9f24307b9c1d4d1e18bba60fac17
Justin Li <justinli4002@gmail.com>
2023-07-20 22:22:06 -0500

    data is bad -- lots of missing values so dropping those missing values leads to very few viable rows causing many problems

--- src/dataset2.py
+++ src/dataset2.py
@@ -1,5 +1,19 @@
 from main import *
 
+def preprocess(data):
+    # https://pandas.pydata.org/docs/reference/api/pandas.get_dummies.html
+    # https://datascience.stackexchange.com/questions/27957/why-do-we-need-to-discard-one-dummy-variable/27993#27993
+    data = pd.get_dummies(data, columns=['category_code', 'country_code', 'state_code'], drop_first=True)
+
+    # Convert boolean features to binary values (0 and 1)
+    data['ipo'] = data['ipo'].astype(int)
+    
+    # try dropping if company is closed or not
+    data['is_closed'] = data['is_closed'].astype(int)
+    # data = data.drop(columns=['is_closed'])
+    
+    return data
+
 if __name__ == '__main__':
     data_path = 'src\\data\\data2.csv'
     data = pd.read_csv(data_path)
@@ -8,6 +22,15 @@ if __name__ == '__main__':
         'company_id'
     ])
     
+    data = preprocess(data)
+    na_count_per_row = data.isna().sum(axis=1)
+
+    # Total number of rows with NaN values
+    total_rows_with_na = len(na_count_per_row[na_count_per_row > 0])
+    print("Total rows with NaN values:", total_rows_with_na)
+    print(len(data))
+    data = data.dropna()
+    
     predictors = data.columns.tolist()
     predictors.remove('is_acquired')
     
@@ -15,10 +38,10 @@ if __name__ == '__main__':
     y = data['is_acquired']
     
     # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=None)
 
 
     # https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
-    model = LogisticRegression(max_iter=300)
+    model = LogisticRegression(max_iter=500)
 
-    run_model(model, 'Logistic', X_train, X_test, y_train, y_test)
+    run_model(model, 'Logistic', X_train, X_test, y_train, y_test, predictors)
