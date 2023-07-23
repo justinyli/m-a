4ed5df9eabec70c436f098fa6685047cc570d035
Justin Li <justinli4002@gmail.com>
2023-07-23 14:15:13 -0500

    added some sample headlines

--- src/sent_analysis.py
+++ src/sent_analysis.py
@@ -9,17 +9,23 @@ def sentiment_analysis(text):
     result = classifier(text)
     label = result[0]['label']
     score = result[0]['score']
-
+    
     return label, score
 
 if __name__ == "__main__":
     # written myself
     headlines = [
-        "Microsoft and Activision in advanced talks",
-        "Sony and Activision not seeing eye to eye",
-        "Acquisition of XYZ Corp by ABC Inc finalized",
-        "Merger between Company A and Company B rumored",
-        "Tech giants explore potential merger",
+        "Microsoft-Activision deal back in hands of UK regulator after court pauses appeal",
+        "SBB, Brookfield end talks on EduCo stake sale, shares tumble",
+        "Safran agrees to buy Collins flight controls business",
+        "FTC to pause Microsoft-Activision merger trial, Bloomberg reports",
+        "Bain Capital raises SoftwareOne takeover offer to $3.7 billion, Bloomberg reports",
+        "Ocado not pursuing takeover offers, says boss",
+        "Investors in VinFast's SPAC cash out most shares",
+        "Chindata says largest shareholder Bain will not sell stake after rival bid",
+        "KKR boosts chemicals portfolio with $1.3 billion deal for Chase",
+        "Investor group nears $125 million deal for CoinDesk, Wall Street Journal reports",
+        "Japan's Sosei buys Idorsia Pharma's Japan, Korea units for $466 mln"
     ]
     
     for i, headline in enumerate(headlines, 1):
